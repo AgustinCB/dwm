a78bbe679373e9130d6bbba566c7828796ebbd31
Agustin Chiappe Berrini <agustin@chiappeberrini.me>
2022-04-08 06:14:13 -0700

    Allow alternative bar

--- config.def.h
+++ config.def.h
@@ -1,11 +1,14 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int cornerrad = 5;
 static const unsigned int showbar   = 1;        /* 0 means no bar */
 static const unsigned int topbar    = 1;        /* 0 means bottom bar */
+static const unsigned int usealtbar = 0;
+static const char *altbarclass	    = "Eww";
+static const char *altbarcmd	    = "$HOME/.config/eww/start";
 static const unsigned int gappx     = 15;
 static const char *fonts[]          = { "JetBrains Mono:size=10" };
 static const char dmenufont[]       = "JetBrains Mono:size=10";
