1c8e3f77098f6c433e8cf496ef4c75b26e99320f
Agustin Chiappe Berrini <agustin@chiappeberrini.me>
2022-04-10 06:45:45 -0700

    Default showbar = 0

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int cornerrad = 5;
-static const unsigned int showbar   = 1;        /* 0 means no bar */
+static const unsigned int showbar   = 0;        /* 0 means no bar */
 static const unsigned int topbar    = 1;        /* 0 means bottom bar */
 static const unsigned int usealtbar = 0;
 static const char *altbarclass	    = "Eww";
